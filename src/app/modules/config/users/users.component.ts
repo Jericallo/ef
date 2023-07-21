38c8a4d75ad7c17a7afa857c08837e60da9b9ed8
Santiago0709 <santi@alphadev.io>
2023-07-21 16:45:03 -0600

    cambio acciones
    cambio

--- src/app/modules/config/users/users.component.ts
+++ src/app/modules/config/users/users.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/shared/services/api.service';
 import { MatTableDataSource } from '@angular/material/table';
+import { AddUserDialogComponent } from '../add-user-dialog/add-user-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 interface Columns {
   id: number,
   nombre: string,
-  correo: string,
+  correo: string, 
   telefono: number,
 }
 
@@ -18,19 +20,33 @@ export class UsersComponent implements OnInit {
 
   dataSource!: MatTableDataSource<any>;
   usersList = []
-  columnsToDisplay = ['id','nombre', 'correo','telefono'];
+  columnsToDisplay = ['id','nombre', 'correo','telefono', 'acciones'];
 
   columnNames = {
     id: '#',
     nombre: 'Nombre',
-    correo: 'Correo',
+    correo: 'Correo',     
     telefono: 'Telefono',
+    acciones: 'Acciones'
   };
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.getUsers()
+  }
+
+  openAdd(){
+    const dialogRef = this.dialog.open(AddUserDialogComponent, {
+      width: '400px',
+      // Puedes pasar datos adicionales al diálogo si lo necesitas.
+      // Por ejemplo, podrías pasar un objeto con información relacionada con el usuario actualmente seleccionado.
+      // data: { selectedUser: this.selectedUser }
+    });
+  }
+
+  openEdit(){
+
   }
 
   getUsers(){
