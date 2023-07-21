a669017786958a343ad86bbcc52c76bd8c751aa0
alphadevio <gabo@alphadev.io>
2023-07-21 12:18:35 -0600

    DemoMaterialModule
    Se agrego el DemoMaterialModule al modulo de config

--- src/app/modules/config/config.module.ts
+++ src/app/modules/config/config.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users/users.component';
 import { ConfigRoutes } from './config.routing';
 import { RouterModule } from '@angular/router';
+import { DemoMaterialModule } from 'src/app/demo-material-module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(ConfigRoutes)
+    RouterModule.forChild(ConfigRoutes),
+    DemoMaterialModule
   ]
 })
 export class ConfigModule { }
