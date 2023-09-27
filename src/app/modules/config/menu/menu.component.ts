95a92288ae9f6e9887e0679088703d02ec961220
Santiago0709 <santi@alphadev.io>
2023-09-26 21:00:53 -0600

    menu fix

--- src/app/modules/config/menu/menu.component.ts
+++ src/app/modules/config/menu/menu.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { MatCardTitle } from '@angular/material/card';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -9,12 +7,95 @@ import { MatCardTitle } from '@angular/material/card';
 })
 export class MenuComponent implements OnInit {
 
+  //[(ngModel)]="searchTerm"
+  searchTerm: string = '';
   
 
+  usuarios: any[] = [
+    {
+      icon: 'group',
+      descripcion: 'Ver todos',
+      path: 'users'
+    },
+    {
+      icon: 'assignment_ind',
+      descripcion: 'Perfiles',
+      path: 'profiles'
+    },
+    {
+      icon: 'view_module',
+      descripcion: 'Modulos',
+      path: 'modules'
+    }
+  ];
 
+  leyes: any[] = [
+    {
+      icon: 'widgets',
+      descripcion: 'Categorias',
+      path: 'categories'
+    },
+    {
+      icon: 'insert_drive_file',
+      descripcion: 'Documentos',
+      path: 'documents'
+    },
+    {
+      icon: 'text_fields',
+      descripcion: 'Titulos',
+      path: 'titles'
+    },
+    {
+      icon: 'book',
+      descripcion: 'Capitulos',
+      path: 'chapters'
+    },
+    {
+      icon: 'bookmark',
+      descripcion: 'Secciones',
+      path:'sections'
+    },
+    {
+      icon: 'description',
+      descripcion: 'Articulos',
+      path:'articles'
+    },
+    {
+      icon: 'format_align_left',
+      descripcion: 'Parrafos',
+      path:'paragraphs'
+    }
+  ];
+
+  filterUsuarios() {
+    const searchTermLower = this.searchTerm.toLowerCase();
+    if (searchTermLower.includes('usuarios')) {
+      return this.usuarios;
+    } else {
+      return this.usuarios.filter(usuario =>
+        usuario.descripcion.toLowerCase().includes(searchTermLower)
+      );
+    }
+  }
+  
+  filterLeyes() {
+    const searchTermLower = this.searchTerm.toLowerCase();
+    if (searchTermLower.includes('leyes')) {
+      return this.leyes;
+    } else {
+      return this.leyes.filter(ley =>
+        ley.descripcion.toLowerCase().includes(searchTermLower)
+      );
+    }
+  }
+  
+  
+  
+
+  
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
 }
