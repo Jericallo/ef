7bd9b5beabfe78aac934213517cbc0c2a308f259
Santiago0709 <santi@alphadev.io>
2023-10-05 13:05:03 -0600

    intro cards fix

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -22,6 +22,7 @@ import {AutocompleteLibModule} from 'angular-ng-autocomplete';
 import { ResponseComponent } from './response/response.component';
 import { SearchDocumentationComponent } from 'src/app/shared/components/search-documentation/search-documentation.component';
 import { VideoIntroComponent } from './video-intro/video-intro.component';
+import { CountdownModalComponent } from './intro/countdown-modal/countdown-modal.component';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { VideoIntroComponent } from './video-intro/video-intro.component';
     ResponseComponent,
     SearchDocumentationComponent,
     VideoIntroComponent,
+    CountdownModalComponent,
   ],
   imports: [
     CommonModule,
